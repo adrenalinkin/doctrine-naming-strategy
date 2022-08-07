bd63b00c41dae231306c2b5875278854797b2734
Viktor Linkin <adrenalinkin@gmail.com>
2022-08-07 23:17:41 +0300

    Declare strategy explicitly to avoid different SQL in the newest versions.

--- Tests/Functional/Entity/Comment.php
+++ Tests/Functional/Entity/Comment.php
@@ -24,7 +24,7 @@ class Comment
 {
     /**
      * @ORM\Id()
-     * @ORM\GeneratedValue()
+     * @ORM\GeneratedValue(strategy="UUID")
      * @ORM\Column(type="string")
      */
     private $id;
--- Tests/Functional/Entity/User.php
+++ Tests/Functional/Entity/User.php
@@ -24,7 +24,7 @@ class User
 {
     /**
      * @ORM\Id()
-     * @ORM\GeneratedValue()
+     * @ORM\GeneratedValue(strategy="UUID")
      * @ORM\Column(type="string")
      */
     private $id;
